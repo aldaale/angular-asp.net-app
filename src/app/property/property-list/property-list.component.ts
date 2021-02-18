c2baa55cd120ce6bfd4fbfb1d078d83b720f1570
Aleksandra Dahlqvist <s2000575@edu.bc.fi>
2021-02-18 15:20:04 +0200

    add databinding

--- src/app/property/property-list/property-list.component.ts
+++ src/app/property/property-list/property-list.component.ts
@@ -7,6 +7,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PropertyListComponent implements OnInit {
 
+  properties: Array<any> = [
+    {
+    "Id":1,
+    "Name": "Birla House",
+    "Type":"House",
+    "Price":19999
+   },
+    {
+    "Id":2,
+    "Name": "Erose Villa",
+    "Type":"House",
+    "Price":13100
+   },
+    {
+    "Id":3,
+    "Name": "Mark Hill",
+    "Type":"House",
+    "Price":12500
+   },
+    {
+    "Id":4,
+    "Name": "Gun Hill",
+    "Type":"House",
+    "Price":11030
+   },
+    {
+    "Id":5,
+    "Name": "Marco House",
+    "Type":"House",
+    "Price":143000
+   },
+    {
+    "Id":6,
+    "Name": "Pearl White House",
+    "Type":"House",
+    "Price":184000
+   }
+
+]
   constructor() { }
 
   ngOnInit(): void {
